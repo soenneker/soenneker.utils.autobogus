285280059ac636479a02269216b33e31d4f13cc6
Jake Soenneker <jake@soenneker.com>
2025-01-23 10:15:20 -0600

    FluentAssertions -> AwesomeAssertions

--- test/Soenneker.Utils.AutoBogus.Tests/Benchmarking/Benchmarks/GenerateRunner.cs
+++ test/Soenneker.Utils.AutoBogus.Tests/Benchmarking/Benchmarks/GenerateRunner.cs
@@ -1,8 +1,10 @@
 using System.Threading.Tasks;
 using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
+using Soenneker.Benchmarking.Extensions.Summary;
 using Soenneker.Facts.Local;
 using Soenneker.Facts.Manual;
+using Soenneker.Tests.Benchmark;
 using Xunit;
 
 namespace Soenneker.Utils.AutoBogus.Tests.Benchmarking.Benchmarks;
@@ -20,7 +22,7 @@ public class GenerateRunner : BenchmarkTest
     {
         Summary summary = BenchmarkRunner.Run<GenerateBenchmarks>(DefaultConf);
 
-        await OutputSummaryToLog(summary, TestContext.Current.CancellationToken);
+        await summary.OutputSummaryToLog(OutputHelper, CancellationToken);
     }
 
     [ManualFact]
@@ -28,7 +30,7 @@ public class GenerateRunner : BenchmarkTest
     {
         Summary summary = BenchmarkRunner.Run<GenerateTBenchmarks>(DefaultConf);
 
-        await OutputSummaryToLog(summary, TestContext.Current.CancellationToken);
+        await summary.OutputSummaryToLog(OutputHelper, CancellationToken);
     }
 
     [ManualFact]
@@ -36,6 +38,6 @@ public class GenerateRunner : BenchmarkTest
     {
         Summary summary = BenchmarkRunner.Run<BogusBenchmarks>(DefaultConf);
 
-        await OutputSummaryToLog(summary, TestContext.Current.CancellationToken);
+        await summary.OutputSummaryToLog(OutputHelper, CancellationToken);
     }
 }
